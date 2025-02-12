285604e96e5c5a1d6abfea8fabffe1e998b57ecc
Hector Alberto Diaz Gomez <146489883+hectordiazgomez@users.noreply.github.com>
2025-02-12 10:58:47 -0800

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from .views import *
+from . import views
 
 urlpatterns = [
-    path('translate/', translation_view, name='translation'),
-    path('get-details/', get_details, name='get_method'),
-    path('math-operation/', math_operation, name='math_operation'),
+    path('train-nmt/', views.train_nmt, name='train_nmt'),
 ]
